177d3b2949079740106ea578d2e184640de46470
kebin20 <ktanzyl@hotmail.com>
2023-01-19 09:22:42 +0900

    2 files modified, 5 added, 1 deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <React.Fragment>
+      <nav>
+        <h1 className={classes.nav__title}>Todo</h1>
+        <button className={classes.nav__toggle}></button>
+      </nav>
+      <header className={classes.header}></header>
+    </React.Fragment>
   );
 }
 
-export default App
+export default App;
