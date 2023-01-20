7dbc18ce3bd9a835555106f586c35af8a3d41956
kebin20 <ktanzyl@hotmail.com>
2023-01-20 09:20:46 +0900

    Created add to do handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import classes from './App.module.css';
 import TodoInput from './components/Todo/TodoInput';
+import TodoList from './components/Todo/TodoList';
 
 function App() {
+  const [todoItem, setTodoItem] = useState([
+    { text: 'Complete online Javascript course', id: '1' },
+    { text: 'Jog around the park 3x', id: '2' },
+  ]);
+
+  function addTodoHandler(enteredText) {
+    setTodoItem((prevTodoItem) => {
+      const updatedTodo = [...prevTodoItem];
+      updatedTodo.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedTodo;
+    });
+  }
+
+  function deleteTodoHandler() {}
+
   return (
     <React.Fragment>
       <header className={classes.header}>
@@ -10,7 +26,7 @@ function App() {
           <h1 className={classes.nav__title}>Todo</h1>
           <button className={classes.nav__togglebutton}></button>
         </nav>
-        <TodoInput />
+        <TodoInput onAddTodo={addTodoHandler}/>
       </header>
     </React.Fragment>
   );
