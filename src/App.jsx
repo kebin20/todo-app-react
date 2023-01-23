4b2d687fa3a5164f887a45437150dc4789afd3da
kebin20 <ktanzyl@hotmail.com>
2023-01-23 15:43:06 +0900

    Added extra items as examples

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
     { text: 'Complete online Javascript course', id: '1', isChecked: false },
     { text: 'Jog around the park 3x', id: '2', isChecked: false },
     { text: '10 minutes meditation', id: '3', isChecked: false },
+    { text: 'Read for 1 hour', id: '4', isChecked: false },
+    { text: 'Pick up groceries', id: '5', isChecked: false },
+    { text: 'Complete Todo App on Frontend Mentor', id: '6', isChecked: false },
   ]);
 
   function addTodoHandler(enteredText) {
