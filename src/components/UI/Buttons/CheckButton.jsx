c4a0e095a561c8592f3fd8895887d777ea8eda84
kebin20 <ktanzyl@hotmail.com>
2023-01-23 14:29:50 +0900

    2 files modified, 1 added

--- src/components/UI/Buttons/CheckButton.jsx
+++ src/components/UI/Buttons/CheckButton.jsx
@@ -3,7 +3,10 @@ import classes from './CheckButton.module.css';
 
 function CheckButton(props) {
   const tickStyle = {
-    background: props.isChecked ? 'linear-gradient(0deg, rgba(34,193,195,0.4234068627450981) 0%, rgba(186,117,190,0.773546918767507) 52%)' : 'transparent',
+    background: props.isChecked
+      ? `linear-gradient(153deg, rgba(34,193,195,0.4234068627450981) 0%, rgba(73,173,194,1) 27%, rgba(186,117,190,0.773546918767507) 58%)`
+      : 'transparent',
+    backgroundImage: props.isChecked ? "url(./tick-button.svg)" : "none",
   };
 
   return (
