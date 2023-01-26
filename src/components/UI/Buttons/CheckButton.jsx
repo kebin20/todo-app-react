c6bb63bb9096b7b92281cd3f3cb6836eade66386
kebin20 <ktanzyl@hotmail.com>
2023-01-26 17:47:58 +0900

    Fixed tick button image

--- src/components/UI/Buttons/CheckButton.jsx
+++ src/components/UI/Buttons/CheckButton.jsx
@@ -6,7 +6,7 @@ import tickButton from "./assets/icon-check.svg";
 function CheckButton(props) {
   const tickStyle = {
     background: props.isChecked
-      ? `linear-gradient(157deg, rgba(34,193,195,0.1460870343839542) 0%, rgba(73,173,194,0.15754835243553011) 27%, rgba(186,117,190,0.3495254297994269) 58%), url(${tickButton})`
+      ? `linear-gradient(157deg, rgba(34,193,195,0.1460870343839542) 0%, rgba(73,173,194,0.15754835243553011) 27%, rgba(186,117,190,0.3495254297994269) 58%), center no-repeat url(${tickButton})`
       : "transparent",
   };
 
