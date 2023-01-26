3787d7174b57f3fb230008d0c4c7d88d8c2caea0
kebin20 <ktanzyl@hotmail.com>
2023-01-26 19:56:20 +0900

    Fixed bug where the tick was nto showing up in light mode

--- src/components/UI/Buttons/CheckButton.jsx
+++ src/components/UI/Buttons/CheckButton.jsx
@@ -1,12 +1,20 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useContext } from "react";
+
+import { ThemeContext } from "../../../themeContext";
+
 import classes from "./CheckButton.module.css";
-import tickButton from "./assets/icon-check.svg";
+import tickButtonDarkMode from "./assets/icon-check-darkmode.svg";
+import tickButtonLightMode from "./assets/icon-check-lightmode.svg";
 
 function CheckButton(props) {
+  const { theme } = useContext(ThemeContext);
+
   const tickStyle = {
     background: props.isChecked
-      ? `linear-gradient(157deg, rgba(34,193,195,0.1460870343839542) 0%, rgba(73,173,194,0.15754835243553011) 27%, rgba(186,117,190,0.3495254297994269) 58%), center no-repeat url(${tickButton})`
+      ? `linear-gradient(157deg, rgba(34,193,195,0.1460870343839542) 0%, rgba(73,173,194,0.15754835243553011) 27%, rgba(186,117,190,0.3495254297994269) 58%), center no-repeat url(${
+          theme === "dark" ? tickButtonDarkMode : tickButtonLightMode
+        })`
       : "transparent",
   };
 
