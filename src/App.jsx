d59647b6b55cc9cf639676c06dfa04ca627ea1ee
kebin20 <ktanzyl@hotmail.com>
2023-01-30 09:19:43 +0900

    Enabled todoitems to be stored in localStorage, needs testing in local environemnt as it is not working in stackblitz

--- src/App.jsx
+++ src/App.jsx
@@ -12,27 +12,27 @@ import initialTodos from './todos';
 import './App.css';
 
 function App() {
-  const [todoItem, setTodoItem] = useState([
-    { text: "Complete online Javascript course", id: "1", isChecked: false },
-    { text: "Jog around the park 3x", id: "2", isChecked: false },
-    { text: "10 minutes meditation", id: "3", isChecked: false },
-    { text: "Read for 1 hour", id: "4", isChecked: false },
-    { text: "Pick up groceries", id: "5", isChecked: false },
-    { text: "Complete Todo App on Frontend Mentor", id: "6", isChecked: false },
-  ]);
+  // const [todoItem, setTodoItem] = useState([
+  //   { text: "Complete online Javascript course", id: "1", isChecked: false },
+  //   { text: "Jog around the park 3x", id: "2", isChecked: false },
+  //   { text: "10 minutes meditation", id: "3", isChecked: false },
+  //   { text: "Read for 1 hour", id: "4", isChecked: false },
+  //   { text: "Pick up groceries", id: "5", isChecked: false },
+  //   { text: "Complete Todo App on Frontend Mentor", id: "6", isChecked: false },
+  // ]);
 
-  // const [todoItem, setTodoItem] = useState(initialTodos);
+  const [todoItem, setTodoItem] = useState(initialTodos);
 
-  // useEffect(() => {
-  //   const storedTodo = localStorage.getItem('todoItem');
-  //   if (storedTodo) {
-  //     setTodoItem(JSON.parse(storedTodo));
-  //   }
-  // }, []);
+  useEffect(() => {
+    const storedTodo = JSON.parse(localStorage.getItem('todoItem'));
+    if (storedTodo) {
+      setTodoItem(storedTodo);
+    }
+  }, []);
 
-  // useEffect(() => {
-  //   localStorage.setItem('todoItem', JSON.stringify(todoItem));
-  // }, [todoItem]);
+  useEffect(() => {
+    localStorage.setItem('todoItem', JSON.stringify(todoItem));
+  }, [todoItem]);
 
   const [visibility, setVisibility] = useState('all');
 
