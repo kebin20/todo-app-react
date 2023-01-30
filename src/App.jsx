a3e4dbfd0af0607ec7003cd39054aafaf87a0089
kebin20 <ktanzyl@hotmail.com>
2023-01-30 16:37:12 +0900

    Deleted old code

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,6 @@ import initialTodos from './todos';
 import './App.css';
 
 function App() {
-  // const [todoItem, setTodoItem] = useState([
-  //   { text: "Complete online Javascript course", id: "1", isChecked: false },
-  //   { text: "Jog around the park 3x", id: "2", isChecked: false },
-  //   { text: "10 minutes meditation", id: "3", isChecked: false },
-  //   { text: "Read for 1 hour", id: "4", isChecked: false },
-  //   { text: "Pick up groceries", id: "5", isChecked: false },
-  //   { text: "Complete Todo App on Frontend Mentor", id: "6", isChecked: false },
-  // ]);
-
   const [todoItem, setTodoItem] = useState(initialTodos);
 
   useEffect(() => {
