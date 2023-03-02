a88130b57cab05927a069808901b3db094bbd383
kebin20 <ktanzyl@hotmail.com>
2023-03-02 18:30:03 +0900

    test: attempted to use firebase to store Todo items, but encountered problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,32 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { ThemeContext } from './themeContext';
+import React, { useState, useEffect, useContext } from "react";
+import { ThemeContext } from "./themeContext";
 
 /* components */
-import TodoInput from './components/Todo/TodoInput';
-import TodoList from './components/Todo/TodoList';
-import TodoOptionBar from './components/Todo/TodoOptionBar';
-import ToggleThemeButton from './components/UI/Buttons/ToggleThemeButton';
+import TodoInput from "./components/Todo/TodoInput";
+import TodoList from "./components/Todo/TodoList";
+import TodoOptionBar from "./components/Todo/TodoOptionBar";
+import ToggleThemeButton from "./components/UI/Buttons/ToggleThemeButton";
 
-import initialTodos from './todos';
+import initialTodos from "./todos";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   const [todoItem, setTodoItem] = useState(initialTodos);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const storedTodo = JSON.parse(localStorage.getItem('todoItem'));
-    if (storedTodo) {
-      setTodoItem(storedTodo);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('todoItem', JSON.stringify(todoItem));
-  }, [todoItem]);
-
-  const [visibility, setVisibility] = useState('all');
+  /* Change theme function */
+  const [visibility, setVisibility] = useState("all");
 
   const { theme } = useContext(ThemeContext);
 
   useEffect(() => {
     document.body.style.backgroundColor =
-      theme === 'light' ? 'white' : 'hsl(235, 21%, 11%)';
+      theme === "light" ? "white" : "hsl(235, 21%, 11%)";
   }, [theme]);
 
+  /* Editing functions */
   function addTodoHandler(enteredText) {
     setTodoItem((prevTodoItem) => {
       const updatedTodo = [...prevTodoItem];
@@ -72,16 +65,105 @@ function App() {
   }
 
   function showAllItems() {
-    setVisibility('all');
+    setVisibility("all");
   }
 
   function showActiveItems() {
-    setVisibility('active');
+    setVisibility("active");
   }
 
   function showCompletedItems() {
-    setVisibility('completed');
+    setVisibility("completed");
   }
+
+  /* Fetching todo function */
+
+  useEffect(() => {
+    const storedTodo = JSON.parse(localStorage.getItem("todoItem"));
+    if (storedTodo) {
+      setTodoItem(storedTodo);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todoItem", JSON.stringify(todoItem));
+  }, [todoItem]);
+
+  /* FIREBASE METHOD */
+  // const fetchTodoHandler = useCallback(async () => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://react-todo-ca214-default-rtdb.firebaseio.com/todo.json"
+  //     );
+  //     if (!response.ok) {
+  //       throw new Error("An error has occurred");
+  //     }
+
+  //     const data = await response.json();
+
+  //     setTodoItem(data);
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  //   setIsLoading(false);
+  // }, []);
+
+  // useEffect(() => {
+  //   fetchTodoHandler();
+  // }, [fetchTodoHandler]);
+
+  // /* Storing initial todoitems */
+  // useEffect(() => {
+  //   setInitialTodoData();
+  // }, []);
+
+  // /* Posting data to Firebase */
+  // const setInitialTodoData = useCallback(async () => {
+  //   const response = await fetch(
+  //     "https://react-todo-ca214-default-rtdb.firebaseio.com/todo.json",
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify(initialTodos),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   setTodoItem(data);
+  // }, []);
+
+  // /* Error Handling */
+
+  // let content = <p>Found no todos.</p>;
+
+  // if (todoItem.length > 0) {
+  //   content = (
+  //     <TodoList
+  //       items={todoItem.filter(
+  //         (item) =>
+  //           visibility === "all" ||
+  //           (visibility === "active" && !item.isChecked) ||
+  //           (visibility === "completed" && item.isChecked)
+  //       )}
+  //       onCheckItem={(id) => checkItem(id)}
+  //       onDeleteItem={deleteTodoHandler}
+  //       onClearCompleted={clearCompletedItems}
+  //     />
+  //   );
+  // }
+
+  // console.log(todoItem);
+
+  // if (error) {
+  //   content = <p>{error}</p>;
+  // }
+
+  // if (isLoading) {
+  //   content = <p>Loading...</p>;
+  // }
 
   return (
     <React.Fragment>
@@ -99,14 +181,15 @@ function App() {
           <TodoList
             items={todoItem.filter(
               (item) =>
-                visibility === 'all' ||
-                (visibility === 'active' && !item.isChecked) ||
-                (visibility === 'completed' && item.isChecked)
+                visibility === "all" ||
+                (visibility === "active" && !item.isChecked) ||
+                (visibility === "completed" && item.isChecked)
             )}
             onCheckItem={(id) => checkItem(id)}
             onDeleteItem={deleteTodoHandler}
             onClearCompleted={clearCompletedItems}
           />
+          {/* {content} */}
           <TodoOptionBar
             onShowAllItems={showAllItems}
             onShowActiveItems={showActiveItems}
